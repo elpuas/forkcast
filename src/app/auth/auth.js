ae1109d2e89659c7684733f9dc8e19f0d06a1c80
Alfredo Navas <elpuas@gmail.com>
2023-12-04 07:36:37 -0600

    Authors logic  Changes to be committed: 	modified:   src/app/auth/auth.js

--- src/app/auth/auth.js
+++ src/app/auth/auth.js
@@ -1,11 +1,25 @@
 import { auth } from '../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
-// Function to sign up a new user with email and password
+// Initialize Firestore
+const db = getFirestore();
+
+
 export const signUpWithEmailPassword = async (email, password) => {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         console.log("User created: ", userCredential.user);
+
+        // Create an author document
+        const authorRef = doc(db, "authors", userCredential.user.uid);
+        await setDoc(authorRef, {
+            password: password,
+            email: userCredential.user.email,
+            createdAt: new Date(),
+        });
+
+        console.log("Author document created for: ", userCredential.user.uid);
     } catch (error) {
         console.error("Error signing up: ", error.message);
     }
